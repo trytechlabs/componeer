a81b52449a50470110a015c5b78892fffcd4b1e9
André Rodrigues <andrerpbts@gmail.com>
2024-05-26 13:57:40 -0300

    List deployment details

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,15 @@
 set -o errexit
 
 echo 'Current directory: ' $PWD
+echo 'Environment: ' $RAILS_ENV
+echo 'Ruby version: ' $(ruby -v)
+echo 'Bundler version: ' $(bundle -v)
+echo 'Node version: ' $(node -v)
+echo 'Yarn version: ' $(yarn -v)
+echo '-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-'
+echo 'Project files tree:'
+ls -la
+echo '-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-'
 echo 'Installing dependencies'
 echo 'Copying credentials...'
 
