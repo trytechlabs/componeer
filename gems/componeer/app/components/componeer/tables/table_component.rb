b8385cce0284f172be8b100c7f130943ee2ee34d
Mauricio Lima <mauricio@collateralxp.com>
2024-06-02 14:05:47 -0300

    add default td classes

--- gems/componeer/app/components/componeer/tables/table_component.rb
+++ gems/componeer/app/components/componeer/tables/table_component.rb
@@ -24,20 +24,32 @@ module Componeer
                                 'border-b border-gray-200 py-2'.freeze
       DEFAULT_TABLE_CLASSES = 'min-w-full divide-y divide-gray-200 bg-white'.freeze
       DEFAULT_TH_CLASSES = 'px-3 py-2 text-xs font-medium uppercase tracking-wider'.freeze
+      DEFAULT_TD_CLASSES = 'px-3 py-2 text-xs font-base tracking-wider'.freeze
 
       def initialize(records: nil, striped: true, options: {})
         @records = records
         @striped = striped
         @wrapper_classes = options.delete(:wrapper_classes) || DEFAULT_WRAPPER_CLASSES
         @table_classes = options.delete(:table_classes) || DEFAULT_TABLE_CLASSES
-        @tr_classes = options.delete(:tr_classes) || ''
+        @tr_classes = options.delete(:tr_classes) || default_tr_classes
         @th_classes = options.delete(:th_classes) || DEFAULT_TH_CLASSES
-        @td_classes = options.delete(:td_classes) || ''
+        @td_classes = options.delete(:td_classes) || DEFAULT_TD_CLASSES
         @options = options
       end
 
       def column_th_classes(column)
         "#{th_classes} #{column.classes_for(:th)}"
+      end
+
+      def column_td_classes(column)
+        "#{td_classes} #{column.classes_for(:td)}"
+      end
+
+      def default_tr_classes(record = nil)
+        bg_color = striped ? cycle('bg-white', 'bg-gray-50') : 'bg-white'
+        tr_class = tr_classes.is_a?(Proc) ? tr_classes.call(record) : tr_classes
+
+        [bg_color, tr_class].compact_blank.uniq.join(' ')
       end
     end
   end
