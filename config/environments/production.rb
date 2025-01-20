352d83b0f7994bb06931afef89cb721c69128bc9
Mauricio Lima <mauricio.prl42@gmail.com>
2025-01-20 11:31:36 -0300

    Bump rails to 8.0.1 (#73)
    * Bump rails to 8.0.1
    
    * run app:update
    
    * change load defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+  config.public_file_server.headers = { 'cache-control' => "public, max-age=#{1.year.to_i}" }
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -55,14 +57,30 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
+  config.logger   = ActiveSupport::TaggedLogging.logger($stdout)
 
   # "info" includes generic and useful information about system operation, but avoids logging too
   # much information to avoid inadvertent exposure of personally identifiable information (PII).
   # If you want to log everything, set the level to "debug".
   config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
 
+  # Prevent health checks from clogging up the logs.
+  config.silence_healthcheck_path = '/up'
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  # Set host to be used by links generated in mailer templates.
+
+  config.action_mailer.default_url_options = { host: 'example.com' }
+
+  # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
+  # config.action_mailer.smtp_settings = {
+  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
+  #   password: Rails.application.credentials.dig(:smtp, :password),
+  #   address: "smtp.example.com",
+  #   port: 587,
+  #   authentication: :plain
+  # }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
