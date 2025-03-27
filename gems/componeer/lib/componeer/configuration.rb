2de8d373384608c725e87bdd0a4565df8a514ad0
André Rodrigues <andrerpbts@gmail.com>
2025-03-27 16:02:20 -0300

    Fix rubocop

--- gems/componeer/lib/componeer/configuration.rb
+++ gems/componeer/lib/componeer/configuration.rb
@@ -3,7 +3,9 @@ module Componeer
     attr_writer :include_componeer_helpers
 
     def include_componeer_helpers?
-      @include_componeer_helpers.nil? ? true : @include_componeer_helpers.present?
+      @include_componeer_helpers ||= true
+
+      @include_componeer_helpers.present?
     end
   end
 end
